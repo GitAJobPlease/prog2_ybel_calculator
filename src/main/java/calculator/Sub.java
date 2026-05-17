e15f95c54ab9ca08557d65ebc77dc60a4cde55fc
foe <janastruckmann6@gmail.com>
2026-05-17 14:51:55 +0200

    Calculator erweitert

---
+++ src/main/java/calculator/Sub.java
@@ -0,0 +1,10 @@
+package calculator;
+
+/** Simple subtraction of two integers. */
+public class Sub implements Operation {
+
+    @Override
+    public int doOperation(int a, int b) {
+        return a - b;
+    }
+}
